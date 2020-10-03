26fa9fb97b38e8f35bc048494faad43749603da4
chaiwenxin1 <2249816238@qq.com>
2020-10-03 19:59:47 +0800

    zuoye

---
+++ build/precache-manifest.01d5667852a1a4b9306500d23d080cf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b340f8334f298a6ec6d9a08fb95da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b766cac13db4c6841089",
+    "url": "/static/js/2.fdf38fef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdf38fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bfca8b27464081b3d12",
+    "url": "/static/js/main.5ce50160.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
