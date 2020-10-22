70283e08c8e67b6f0ebc3cdb6660bfa64939edd8
chaiwenxin1 <2249816238@qq.com>
2020-10-22 20:23:23 +0800

    test

---
+++ build1/build/precache-manifest.b6e84d0319b38f77e62020dadd139742.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7262e80fcd3243e9aced047667a656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4ab5e2392e68c2abaf1",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "69af2f9f2488ce2870e3",
+    "url": "/static/css/main.6931eed1.chunk.css"
+  },
+  {
+    "revision": "e4ab5e2392e68c2abaf1",
+    "url": "/static/js/2.c80d72b6.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.c80d72b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69af2f9f2488ce2870e3",
+    "url": "/static/js/main.d3ee3622.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "127b1edc00be078ea57a73552d32f381",
+    "url": "/static/media/100-0-lubanu.127b1edc.jpg"
+  },
+  {
+    "revision": "daaf4d60479fd0c22794c3a0adc1d780",
+    "url": "/static/media/14-0-luban.daaf4d60.jpg"
+  },
+  {
+    "revision": "60b4732520a566884758b18750c9bbf8",
+    "url": "/static/media/208-0-picassobanner.60b47325.jpg"
+  },
+  {
+    "revision": "adf7c28e5c51f339b984ae71edee0da1",
+    "url": "/static/media/27-0-luban.adf7c28e.jpg"
+  },
+  {
+    "revision": "9beaa82725efa95942ac031da9a19d85",
+    "url": "/static/media/41-0-ppp-picassogw.9beaa827.jpg"
+  },
+  {
+    "revision": "b64a2164291b19f1ccd19a53a1cc5ec9",
+    "url": "/static/media/746982544.jpg_760x760Q50s50.jpg_.b64a2164.webp"
+  },
+  {
+    "revision": "5b3960041be1ee18791045059c8a0e56",
+    "url": "/static/media/80-0-lubanu.5b396004.jpg"
+  },
+  {
+    "revision": "88d5d6e82b095ffdd359b8a55fb2da2c",
+    "url": "/static/media/96-0-luban.88d5d6e8.jpg"
+  },
+  {
+    "revision": "fb6da3f5586e6e031a175a454ccfc324",
+    "url": "/static/media/TB15tIjGVXXXXcoapXXSutbFXXX.fb6da3f5.jpg"
+  }
+]);
